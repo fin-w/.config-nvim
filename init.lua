8baa62a391e094eca14535e8e0fc954abec5629f
fin-w <fin-w@tutanota.com>
2025-03-15 12:49:39 +0000

    set up lualine and move visual to relevant file

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = ' '
 require('config.lazy')
-vim.cmd.colorscheme 'midnight'
 
 require('init.indents')
 require('init.misc')
@@ -15,4 +14,5 @@ require('config.treesitter')
 require('config.telescope')
 require('config.lspconfig')
 require('config.nvim_cmp')
+require('config.lualine')
 -- vim.lsp.enable('rust_analyzer') -- this should trigger the lsp/rust_analyzer.lua file config to run and should set up the autocmd on buffer open i think but it doesn't https://lsp-zero.netlify.app/blog/lsp-config-without-plugins.html
