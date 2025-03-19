a404b576e65b34829d4a4b2fca1c83af7e29b4be
fin-w <fin-w@tutanota.com>
2025-03-19 02:37:35 +0000

    fix tabbing when no suggestion (i.e. just add tab)

--- lua/config/nvim_cmp.lua
+++ lua/config/nvim_cmp.lua
@@ -19,13 +19,20 @@ cmp.setup({
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ['<Tab>'] = cmp.mapping(function(fallback)
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and
+                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
             if cmp.visible() then
                 cmp.select_next_item()
                 -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
                 -- they way you will only jump inside the snippet region
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif cmp.has_words_before() then
+            elseif has_words_before() then
                 cmp.complete()
             else
                 fallback()
