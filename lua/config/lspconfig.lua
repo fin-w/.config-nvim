560f665b6581de3ba6992a69ac8201a4ed88797c
fin-w <fin-w@tutanota.com>
2025-05-09 02:13:37 +0100

    Add config for Bash and XML, improve installation comments

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -43,11 +43,25 @@ lspconfig.jsonls.setup({
 --     capabilities = lsp_capabilities,
 -- })
 
+-- npm install -g vscode-langservers-extracted
+-- or
+-- npm install --save vscode-css-languageservice
 lspconfig.cssls.setup({
     capabilities = lsp_capabilities,
 })
 
+-- npm install -g fish-lsp
 lspconfig.fish_lsp.setup({
+    capabilities = lsp_capabilities,
+})
+
+-- sudo pacman -Syu bash-language-server shfmt
+lspconfig.bashls.setup({
+    capabilities = lsp_capabilities,
+})
+
+-- wget 'https://github.com/redhat-developer/vscode-xml/releases/download/0.28.1/lemminx-linux.zip' && unzip lemminx-linux.zip && mv lemminx-linux ~/.local/bin/lemminx
+lspconfig.lemminx.setup({
     capabilities = lsp_capabilities,
 })
 
