7528de21ff8de30c43166f34a02a19e1be09c576
fin-w <fin-w@tutanota.com>
2025-05-21 15:19:12 +0100

    cleaner plugins list

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,16 +1,41 @@
 return {
-    'fin-w/midnight.nvim',                                                                                                                     -- colour scheme
-    { 'nvim-lualine/lualine.nvim',                  dependencies = { 'nvim-tree/nvim-web-devicons' } },                                        -- bottom ui line
-    'nat-418/boole.nvim',                                                                                                                      -- toggle various related words
-    'lewis6991/gitsigns.nvim',                                                                                                                 -- status column git diff
-    'tpope/vim-fugitive',                                                                                                                      -- all things git
-    { 'nvim-telescope/telescope.nvim',              dependencies = { 'nvim-lua/plenary.nvim' } },                                              -- search filenames, grep, treesitter,
-    { 'nvim-telescope/telescope-file-browser.nvim', dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' } },
-    { 'nucc/git-select-branch',                     dependencies = { 'nvim-telescope/telescope.nvim' } },                                      -- telescope pick git branches
-    'nvim-treesitter/nvim-treesitter',                                                                                                         -- better syntax highlighting
-    { 'nvim-treesitter/nvim-treesitter-context', dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' } },                -- display location context at top
-    { 'saghen/blink.cmp',                        dependencies = { 'rafamadriz/friendly-snippets', 'L3MON4D3/LuaSnip' },      version = '1.*' }, -- fast auto-completions
-    { 'neovim/nvim-lspconfig',                   dependencies = { 'saghen/blink.cmp' } },                                                      -- sensible config for built-in neovim LS
-    { 'L3MON4D3/LuaSnip',                        dependencies = { 'rafamadriz/friendly-snippets' } },                                          -- needed for snippets from blink
-    'rafamadriz/friendly-snippets',                                                                                                            -- lots of vscode-like snippets
+    'fin-w/midnight.nvim', -- colour scheme
+    {                      -- bottom ui line
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    'nat-418/boole.nvim',      -- toggle various related words
+    'lewis6991/gitsigns.nvim', -- status column git diff
+    'tpope/vim-fugitive',      -- all things git
+    {                          -- search filenames, grep, treesitter,
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'nvim-telescope/telescope-file-browser.nvim',
+        dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
+    },
+    { -- telescope pick git branches
+        'nucc/git-select-branch',
+        dependencies = { 'nvim-telescope/telescope.nvim' }
+    },
+    'nvim-treesitter/nvim-treesitter', -- better syntax highlighting
+    {                                  -- display location context at top
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
+    },
+    { -- fast auto-completions
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets', 'L3MON4D3/LuaSnip' },
+        version = '1.*'
+    },
+    { -- sensible config for built-in neovim LS
+        'neovim/nvim-lspconfig',
+        dependencies = { 'saghen/blink.cmp' }
+    },
+    { -- needed for snippets from blink
+        'L3MON4D3/LuaSnip',
+        dependencies = { 'rafamadriz/friendly-snippets' }
+    },
+    'rafamadriz/friendly-snippets', -- lots of vscode-like snippets
 }
