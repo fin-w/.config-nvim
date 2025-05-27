6b4fbb5c8433ead5315a43b5e503846ae611fe78
fin-w <fin-w@tutanota.com>
2025-05-27 17:51:46 +0100

    tidy arrangement of plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,13 +1,9 @@
 return {
-    'fin-w/midnight.nvim', -- colour scheme
-    {                      -- bottom ui line
+    { -- bottom ui line
         'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' }
     },
-    'nat-418/boole.nvim',      -- toggle various related words
-    'lewis6991/gitsigns.nvim', -- status column git diff
-    'tpope/vim-fugitive',      -- all things git
-    {                          -- search filenames, grep, treesitter,
+    { -- search filenames, grep, treesitter,
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
@@ -15,8 +11,7 @@ return {
         'nvim-telescope/telescope-file-browser.nvim',
         dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
     },
-    'nvim-treesitter/nvim-treesitter', -- better syntax highlighting
-    {                                  -- display location context at top
+    { -- display location context at top
         'nvim-treesitter/nvim-treesitter-context',
         dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' }
     },
@@ -33,6 +28,11 @@ return {
         'L3MON4D3/LuaSnip',
         dependencies = { 'rafamadriz/friendly-snippets' }
     },
+    'fin-w/midnight.nvim',             -- colour scheme
+    'nat-418/boole.nvim',              -- toggle various related words
+    'lewis6991/gitsigns.nvim',         -- status column git diff
+    'tpope/vim-fugitive',              -- all things git
+    'nvim-treesitter/nvim-treesitter', -- better syntax highlighting
     'rafamadriz/friendly-snippets',    -- lots of vscode-like snippets
     'mfussenegger/nvim-dap',           -- debug adapter protocol
     'theHamsta/nvim-dap-virtual-text', -- virtual text showing contents of variables when using dap
