4cffb804e1281f541559f3c22752e3ac69dd2e0f
fin-w <fin-w@tutanota.com>
2025-06-05 00:06:30 +0100

    convert to lua

--- lua/init/restore.lua
+++ lua/init/restore.lua
@@ -1,6 +1,10 @@
 -- SAVING
+
+-- Save undo history to a file for persistent undo history
+vim.o.undofile = true
 -- Default to saving the session when nvim exits (handled below)
 vim.g.savesession = true
+
 -- Save the session
 vim.api.nvim_create_autocmd('VimLeavePre', {
     pattern = '*',
@@ -10,21 +14,20 @@ vim.api.nvim_create_autocmd('VimLeavePre', {
         end
     end
 })
--- Save undo history to a file for persistent undo history
-vim.cmd([[
-    set undofile                    " save undo history to file so reloading nvim keeps it
-]])
 
 
 -- RESTORE
+
 -- Return to the same line when restoring a file
-vim.cmd([[
-    " Vim jump to the last position when reopening a file
-    if has('autocmd')
-        au BufReadPost * if line("'\"") > 0 && line("'\"") <= line('$')
-            \| exe "normal! g'\"" | endif
-    endif
-]])
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local line_count = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.api.nvim_win_set_cursor(0, { mark[1], mark[2] })
+        end
+    end,
+})
 
 -- If a session exists in the current directory, load it
 local sessionFile = io.open('Session.vim', 'r')
