df8dc3950040ddbc9335847f1656d35e428a7133
fin-w <fin-w@tutanota.com>
2025-06-20 19:35:44 +0100

    use slightly more advanced approach to highlighting

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -24,6 +24,12 @@ require('nvim-treesitter').install(installed_treesitter_parsers)
 require('nvim-treesitter').update()
 vim.api.nvim_create_autocmd({ 'FileType' }, {
     callback = function(context)
-        pcall(vim.treesitter.start, context.bufnr)
+        local language = vim.treesitter.language.get_lang(vim.bo[context.buf].filetype)
+        if not language then return end
+
+        if vim.treesitter.query.get(language, 'highlights') then
+            vim.treesitter.start(context.buf, language)
+            vim.bo[context.buf].syntax = 'ON'
+        end
     end
 })
