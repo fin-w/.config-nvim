d981d0feaaf8755ebb07350352cf89c210f6c16f
fin-w <fin-w@tutanota.com>
2025-06-30 11:49:25 +0100

    disable inlay hints while in insert mode - i would prefer only current line but it's not possible

--- lua/init/lsp.lua
+++ lua/init/lsp.lua
@@ -1,5 +1,7 @@
 -- Enable LSP inlay hinting
-vim.lsp.inlay_hint.enable()
+vim.lsp.inlay_hint.enable(true)
+vim.api.nvim_create_autocmd('InsertEnter', { callback = function() vim.lsp.inlay_hint.enable(false, { bufnr = 0 }) end })
+vim.api.nvim_create_autocmd('InsertLeave', { callback = function() vim.lsp.inlay_hint.enable(true, { bufnr = 0 }) end })
 
 -- Please stop filling my hard drive
 vim.lsp.set_log_level('off')
