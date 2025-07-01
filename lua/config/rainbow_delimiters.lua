afbfb7653e0b8be63debbf1a79ba6420a836f9e2
fin-w <fin-w@tutanota.com>
2025-07-01 22:16:55 +0100

    prefer ensuring rainbow delimiters is correctly loaded on cursor hold, meaning it is not constantly restarting while typing in insert mode

--- lua/config/rainbow_delimiters.lua
+++ lua/config/rainbow_delimiters.lua
@@ -1,19 +1,21 @@
 -- Temporary hack to fix disappearing rainbow delimeters on scroll/CTRL+D
 -- https://github.com/HiPhish/rainbow-delimiters.nvim/issues/174
 local timer = nil
-vim.api.nvim_create_autocmd("WinScrolled", {
+vim.api.nvim_create_autocmd("CursorHold", {
     callback = function()
         if timer then
             timer:stop()
         end
         timer = vim.loop.new_timer()
-        timer:start(
-            250,
-            0,
-            vim.schedule_wrap(function()
-                require("rainbow-delimiters").enable(0)
-                vim.cmd("redraw")
-            end)
-        )
+        if timer then
+            timer:start(
+                250,
+                0,
+                vim.schedule_wrap(function()
+                    require("rainbow-delimiters").enable(0)
+                    vim.cmd("redraw")
+                end)
+            )
+        end
     end,
 })
