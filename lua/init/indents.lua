af72c911c4c804a114dbda248929e193fe885ef4
fin-w <fin-w@tutanota.com>
2025-08-14 20:54:37 +0100

    refactor: use lua instead of vimscript

--- lua/init/indents.lua
+++ lua/init/indents.lua
@@ -1,7 +1,10 @@
-vim.cmd([[
-    set tabstop=4                   " number of cols occupied by <tab>
-    set softtabstop=4               " see multiple spaces as tabstops so <BS> does the right thing
-    set expandtab                   " converts tabs to white space
-    set shiftwidth=4                " width for autoindents
-    set autoindent                  " indent a new line the same amount as the line just typed
-]])
+-- number of cols occupied by <tab>
+vim.opt.tabstop = 4
+-- see multiple spaces as tabstops so <BS> does the right thing
+vim.opt.softtabstop = 4
+-- converts tabs to white space
+vim.opt.expandtab = true
+-- width for autoindents
+vim.opt.shiftwidth = 4
+-- indent a new line the same amount as the line just typed
+vim.opt.autoindent = true
