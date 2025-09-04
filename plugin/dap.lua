7f52806c63e2f4f851d76cc8ef545478e3f8edfc
fin-w <fin-w@tutanota.com>
2025-09-04 12:42:30 +0100

    dap: add lldb option for rust, add rust type displaying (inline and in dap-view), comment out unused dap options

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -6,40 +6,106 @@ dap.adapters.gdb = {
     args = { '--interpreter=dap', '--eval-command', 'set print pretty on' }
 }
 
+dap.adapters.lldb = {
+    type = 'executable',
+    command = 'lldb-dap',
+}
+
 dap.configurations.rust = {
     {
-        name = 'Launch',
-        type = 'gdb',
+        name = 'LLDB',
+        type = 'lldb',
         request = 'launch',
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
         cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        initCommands = function()
+            -- Find out where to look for the pretty printer Python module.
+            local rustc_sysroot = vim.fn.trim(vim.fn.system 'rustc --print sysroot')
+            assert(
+                vim.v.shell_error == 0,
+                'failed to get rust sysroot using `rustc --print sysroot`: '
+                .. rustc_sysroot
+            )
+            local script_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py'
+            local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+            -- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#rust-types
+            -- The following is a table/list of lldb commands, which have a syntax
+            -- similar to shell commands.
+            --
+            -- To see which command options are supported, you can run these commands
+            -- in a shell:
+            --
+            --   * lldb --batch -o 'help command script import'
+            --   * lldb --batch -o 'help command source'
+            --
+            -- Commands prefixed with `?` are quiet on success (nothing is written to
+            -- debugger console if the command succeeds).
+            --
+            -- Prefixing a command with `!` enables error checking (if a command
+            -- prefixed with `!` fails, subsequent commands will not be run).
+            --
+            -- NOTE: it is possible to put these commands inside the ~/.lldbinit
+            -- config file instead, which would enable rust types globally for ALL
+            -- lldb sessions (i.e. including those run outside of nvim). However,
+            -- that may lead to conflicts when debugging other languages, as the type
+            -- formatters are merely regex-matched against type names. Also note that
+            -- .lldbinit doesn't support the `!` and `?` prefix shorthands.
+            return {
+                ([[!command script import '%s']]):format(script_file),
+                ([[command source '%s']]):format(commands_file),
+            }
+        end
+    },
+    {
+        name = 'GDB',
+        type = 'gdb',
+        request = 'launch',
+        program = function()
+            -- require('fzf-lua').files({
+            --     no_ignore = true,
+            --     actions = {
+            --         ['default'] = function(selected_file)
+            --             local filename_from_selected_entry = require('fzf-lua.path').entry_to_file(selected_file[1])
+            --             vim.notify(vim.inspect(filename_from_selected_entry))
+            --             local full_path = vim.fn.fnamemodify(filename_from_selected_entry, ':p')
+            --             vim.notify('Selected: ' .. full_path)
+            --             return full_path
+            --         end,
+            --     },
+            -- })
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+
+        cwd = '${workspaceFolder}',
         stopAtBeginningOfMainSubprogram = false,
     },
-    {
-        name = 'Select and attach to process',
-        type = 'gdb',
-        request = 'attach',
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        pid = function()
-            local name = vim.fn.input('Executable name (filter): ')
-            return require('dap.utils').pick_process({ filter = name })
-        end,
-        cwd = '${workspaceFolder}'
-    },
-    {
-        name = 'Attach to gdbserver :1234',
-        type = 'gdb',
-        request = 'attach',
-        target = 'localhost:1234',
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}'
-    },
+    -- {
+    --     name = 'Select and attach to process',
+    --     type = 'gdb',
+    --     request = 'attach',
+    --     program = function()
+    --         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    --     end,
+    --     pid = function()
+    --         local name = vim.fn.input('Executable name (filter): ')
+    --         return require('dap.utils').pick_process({ filter = name })
+    --     end,
+    --     cwd = '${workspaceFolder}'
+    -- },
+    -- {
+    --     name = 'Attach to gdbserver :1234',
+    --     type = 'gdb',
+    --     request = 'attach',
+    --     target = 'localhost:1234',
+    --     program = function()
+    --         return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    --     end,
+    --     cwd = '${workspaceFolder}'
+    -- },
 }
 
 dap.configurations.cpp = dap.configurations.rust
