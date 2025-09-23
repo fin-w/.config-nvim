c4cf2e31f34f816459f6dec7642dcafe4e2cc416
fin-w <fin-w@tutanota.com>
2025-09-23 20:29:40 +0100

    overseer: set task list width, tasks finish quietly without prompting interaction

--- plugin/overseer.lua
+++ plugin/overseer.lua
@@ -1,1 +1,22 @@
-require('overseer').setup({})
+require('overseer').setup({
+    task_list = {
+        min_width = 55,
+        max_width = 55,
+        width = 55,
+    },
+    component_aliases = {
+        default = {
+            { 'display_duration',    detail_level = 2 },
+            'on_output_summarize',
+            'on_exit_set_status',
+            -- The other settings here are defaults. The important part is
+            -- the commented line below: this avoids logging in :messages
+            -- and stops Neovim prompting for the Enter key when a task
+            -- fails. Overseer uses vim.notify() under the hood and on a
+            -- certain log level (WARN?) it seems to want user interaction.
+            --
+            -- 'on_complete_notify',
+            { 'on_complete_dispose', require_view = { 'SUCCESS', 'FAILURE' } },
+        },
+    },
+})
