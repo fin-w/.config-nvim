a66be33f93f8af74a1df6c24ce6de2a3d9d47030
fin-w <fin-w@tutanota.com>
2025-10-18 06:17:42 +0100

    todo-comments: add plugin to colour important comments better

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ vim.pack.add({
     'https://github.com/stevearc/oil.nvim',                       -- File manager.
     'https://github.com/stevearc/quicker.nvim',                   -- Smarter quickfix list.
     'https://github.com/nvim-mini/mini.clue',                     -- Remind me of my keymaps.
+    'https://github.com/folke/todo-comments.nvim',                -- Pretty notes in my comments.
     'https://github.com/Eandrju/cellular-automaton.nvim',         -- Easter egg :) use <Leader>rain to activate.
 })
 
