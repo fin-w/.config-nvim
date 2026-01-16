93b2cf4cb3bba6a3f6d3582efa0802ff056b8b57
fin-w <fin-w@tutanota.com>
2026-01-16 18:10:54 +0000

    lualine: add cloud icon to sync

--- plugin/lualine.lua
+++ plugin/lualine.lua
@@ -2,8 +2,9 @@
 
 local function network_active()
     if vim.g.network_active == true then
+        -- Font Awesome Cloud icon
         -- Material design Sync icon
-        return '\u{f04e6}'
+        return '\u{f0c2}\u{f04e6}'
     else
         return ''
     end
