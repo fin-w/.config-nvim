8cda76488014091cadb225fd76b4e4be082202de
fin-w <fin-w@tutanota.com>
2026-02-23 00:17:26 +0000

    fix(gutter): set line number highlight to language server / linter feedback
    rather than using a separate gutter, combine line number with linting
    etc by recolouring the line numbers where there's a problem. this broke
    some time ago but i didn't fully notice

--- lua/config/visuals.lua
+++ lua/config/visuals.lua
@@ -74,14 +74,22 @@ vim.g.network_active = false
 vim.diagnostic.config({ virtual_text = { prefix = '◀', } })
 
 -- Diagnostics colours in line numbers rather than in a separate gutter
-for _, diag in ipairs({ 'Error', 'Warn', 'Info', 'Hint' }) do
-    vim.fn.sign_define('DiagnosticSign' .. diag, {
-        text = '',
-        texthl = 'DiagnosticSign' .. diag,
-        linehl = '',
-        numhl = 'DiagnosticSign' .. diag,
-    })
-end
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN]  = '',
+            [vim.diagnostic.severity.INFO]  = '',
+            [vim.diagnostic.severity.HINT]  = '',
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = 'DiagnosticError',
+            [vim.diagnostic.severity.WARN]  = 'DiagnosticWarn',
+            [vim.diagnostic.severity.INFO]  = 'DiagnosticInfo',
+            [vim.diagnostic.severity.HINT]  = 'DiagnosticHint',
+        },
+    },
+})
 
 
 -- MACRO INDICATOR
