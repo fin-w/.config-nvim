68fcb957984933d06454e8ee02fbb12641a8a070
fin-w <fin-w@tutanota.com>
2026-02-23 01:04:54 +0000

    spell: check spelling of git commits in Cymraeg and English

--- lua/config/misc.lua
+++ lua/config/misc.lua
@@ -6,8 +6,17 @@ vim.opt.clipboard:append('unnamedplus')
 vim.opt.wrap = true
 vim.opt.linebreak = true
 
+-- In KDE, *.rc files are XML.
 vim.filetype.add({
     extension = {
         rc = 'xml',
     }
+})
+
+-- Use the Cymraeg and English spell checkers when writing git commit messages.
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'gitcommit',
+    callback = function()
+        vim.cmd [[ setlocal spell spelllang=cy,en ]]
+    end
 })
