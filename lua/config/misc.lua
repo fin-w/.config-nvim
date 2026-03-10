eefb1433b2bf5203e999a9a8d33a75fa59abdb11
fin-w <fin-w@tutanota.com>
2026-03-10 00:30:08 +0000

    fix(proofing): only enable spellcheck and proofing for git commits and markdown files, as intended
    spellcheck and reflow was enabled on buffer when blink popup showed
    because i wasn't filtering properly. new approach doesn't need a delay
    before executing either which is a bonus.

--- lua/config/misc.lua
+++ lua/config/misc.lua
@@ -20,24 +20,27 @@ vim.opt.spelllang = { 'cy', 'en' }
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'gitcommit', 'markdown' },
     callback = function(callback_args)
-        vim.defer_fn(function()
-            -- Don't do anything to popup windows configured as markdown e.g. blink popups.
-            -- Popup windows have `relative` set so we can confirm we want to modify this
-            -- buffer by confirming the variable is empty.
-            if vim.api.nvim_win_get_config(0).relative == '' then
-                -- Use the spell checkers.
-                vim.opt_local.spell = true
-                -- The paragraph reflows as you type.
-                vim.opt_local.formatoptions:append('a')
-                -- Only set the following for commit message buffers.
-                if callback_args.match == 'gitcommit' then
-                    -- Insert some newlines to avoid reflowing the git overview text by accident.
-                    vim.api.nvim_buf_set_lines(0, 0, 0, true, { '', '', '' })
-                    vim.cmd('silent write')
-                    -- Enter insert mode at the top of the buffer.
-                    vim.api.nvim_feedkeys("ggi", "n", false)
-                end
-            end
-        end, 100)
-    end
+        local win = vim.fn.bufwinid(callback_args.buf)
+        if win == -1 then return end
+
+        -- Don't do anything to popup windows configured as markdown e.g. blink popups.
+        -- Popup windows have `relative` set so we can confirm we want to modify this
+        -- buffer by confirming the variable is empty.
+        local cfg = vim.api.nvim_win_get_config(win)
+        if cfg.relative ~= '' then return end
+
+        -- Use the spell checkers.
+        vim.opt_local.spell = true
+        -- The paragraph reflows as you type.
+        vim.opt_local.formatoptions:append('a')
+
+        -- Only set the following for commit message buffers.
+        if callback_args.match == 'gitcommit' then
+            -- Insert some newlines to avoid reflowing the git overview text by accident.
+            vim.api.nvim_buf_set_lines(callback_args.buf, 0, 0, true, { '', '', '' })
+            vim.cmd('silent write')
+            -- Enter insert mode at the top of the buffer.
+            vim.api.nvim_feedkeys('ggi', 'n', false)
+        end
+    end,
 })
