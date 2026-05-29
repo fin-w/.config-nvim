64169e4b2e2816139fd4672cccf9269db96fba33
fin-w <fin-w@tutanota.com>
2026-05-29 22:13:47 +0100

    tiny_inline_diagnostic: better look

--- plugin/tiny_inline_diagnostic.lua
+++ plugin/tiny_inline_diagnostic.lua
@@ -1,9 +1,16 @@
+vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
 require('tiny-inline-diagnostic').setup({
-    preset = 'classic',
-    options = {
-        multilines = {
-            enabled = true,
-        },
+    options = { multilines = { enabled = true } },
+    signs = {
+        left = '',
+        right = '',
+        diag = '●',
+        arrow = '',
+        up_arrow = '',
+        vertical = ' │',
+        vertical_end = ' ╰',
+    },
+    blend = {
+        factor = 0.2,
     },
 })
-vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
